29eb804b71492c6f243f6631c06db81a1cda3861
Andrzej Pultyn <01178581@pw.edu.pl>
2025-11-22 21:33:21 +0100

    Removed seeders from init

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,6 @@
 
 php artisan migrate
 
-php artisan db:seed --class=RoleSeeder
-php artisan db:seed --class=UserSeeder
-php artisan db:seed --class=ReviewSeeder
-
 php-fpm &
 
 exec nginx -g 'daemon off;'
